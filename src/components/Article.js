7de471d94e384932428ebd7c832656c1557262e3
johndwatt <jdw23@protonmail.com>
2022-06-24 19:06:38 -0700

    rearanges box look to include all requested information

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,8 +6,15 @@ function Article(props) {
     return (
         <a href="" className='article-link'>
             <section className='article-box'>
-                <h3 className='article-title'>Title Here: The title is longer</h3>
-                <p className='article-author'>by Author Here</p>
+                <div className='flex'>
+                    <h3 className='article-title'>Title Here: The title is longer</h3>
+                    <p className='article-author'>Date 2022</p>
+                </div>
+                <div className='flex'>
+                    <p className='article-author'>by Author Here</p>
+                    <p className='article-author'>Section</p>
+                </div>
+
             </section>
         </a>
     );
