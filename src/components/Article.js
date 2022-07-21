64a4ef0f82b874bb6a96a9d264b0037110b6744f
johndwatt <jdw23@protonmail.com>
2022-07-21 15:36:19 -0700

    gets proper title, author, and date to display

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,33 +4,6 @@ import "../styles/Article.css"
 
 function Article(props) {
 
-    // const [title, setTitle] = useState("");
-    // const [author, setAuthor] = useState("");
-    // const [date, setDate] = useState("");
-
-    // useEffect(() => {
-    //     const formatDateStr = function (str) {
-    //         const dateObj = new Date(str);
-    //         let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
-    //         return `${months[dateObj.getMonth()]} ${dateObj.getFullYear()}`;
-    //     }
-        
-    //     const formatAuthorStr = function (str) {
-    //         let author = str.match(/by (.*?)</i)[1]
-    //         return author;
-    //     }
-        
-    //     const formatTitleStr = function (str) {
-    //         let title = str.match(/<h1>(.*?)</i)[1]
-    //         return title;
-    //     }
-
-    //     setDate(formatDateStr(props.article.date));
-    //     setTitle(formatTitleStr(props.article.title));
-    //     setAuthor(formatAuthorStr(props.atticle.content.rendered));
-    // }, []);
-    
-
     return (
         <a href={props.link} className='article-link'>
             <section className='article-box'>
@@ -39,8 +12,8 @@ function Article(props) {
                     <p className='article-date'>{props.date}</p>
                 </div>
                 <div className='flex'>
-                    <p className='article-author'>by {props.author}</p>
-                    <p className='article-section'>Section</p>
+                    <p className='article-author'>{props.author}</p>
+                    <p className='article-section'>{props.id}</p>
                 </div>
 
             </section>
--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -11,8 +11,43 @@ function ArticleContainer(props) {
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
-        let temp = JSON.parse(JSON.stringify(DATA_OBJ));
-        setArticles(temp);
+        const formatDateStr = function (str) {
+            const dateObj = new Date(str);
+            let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+            return `${months[dateObj.getMonth()]} ${dateObj.getFullYear()}`;
+        }
+        
+        const formatAuthorStr = function (str) {
+            let author = str.match(/<em>by(.*?)</i);
+            if (author) {
+                return author[1]
+            } else {
+                return "Spotlight";
+            }
+        }
+        
+        const formatTitleStr = function (str, alt) {
+            let title = str.match(/<h1>(.*?)</i);
+            if (title) {
+                return title[1];
+            } else {
+                return alt;
+            }
+        }
+
+        let data = JSON.parse(JSON.stringify(DATA_OBJ));
+
+        let dataMod = data.map((article) => {
+            return {
+                date: formatDateStr(article.date),
+                title: formatTitleStr(article.content.rendered, article.title.rendered),
+                author: formatAuthorStr(article.content.rendered),
+                link: article.link,
+                id: article.id,
+            } 
+        });
+        setArticles(dataMod);
+
     }, []);
 
     console.log(articles);
@@ -25,10 +60,11 @@ function ArticleContainer(props) {
                     // PROBLEM HERE: will not display article because each article is a JS object, need to compare to shower thoughts and see what happens there. How to parse properly so that the data can be gathered?
                     // try to set up auth headers and actually get the real data from wordpress site with your password stuff
                     articles.map((article) => {
+                        // console.log(article);
                         return (
                             <Article 
-                            key={article.id} 
-                            title={article.title.rendered}
+                            key={article.id}
+                            title={article.title}
                             author={article.author}
                             date={article.date}
                             link={article.link} />
