3066bb9709a28794948e0974dae40371a53ca31d
johndwatt <jdw23@protonmail.com>
2022-07-21 14:40:08 -0700

    gets temp data to load properly

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,16 +3,44 @@ import React from 'react';
 import "../styles/Article.css"
 
 function Article(props) {
+
+    // const [title, setTitle] = useState("");
+    // const [author, setAuthor] = useState("");
+    // const [date, setDate] = useState("");
+
+    // useEffect(() => {
+    //     const formatDateStr = function (str) {
+    //         const dateObj = new Date(str);
+    //         let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+    //         return `${months[dateObj.getMonth()]} ${dateObj.getFullYear()}`;
+    //     }
+        
+    //     const formatAuthorStr = function (str) {
+    //         let author = str.match(/by (.*?)</i)[1]
+    //         return author;
+    //     }
+        
+    //     const formatTitleStr = function (str) {
+    //         let title = str.match(/<h1>(.*?)</i)[1]
+    //         return title;
+    //     }
+
+    //     setDate(formatDateStr(props.article.date));
+    //     setTitle(formatTitleStr(props.article.title));
+    //     setAuthor(formatAuthorStr(props.atticle.content.rendered));
+    // }, []);
+    
+
     return (
-        <a href="" className='article-link'>
+        <a href={props.link} className='article-link'>
             <section className='article-box'>
                 <div className='flex'>
-                    <h3 className='article-title'>Title Here: The title is longer</h3>
-                    <p className='article-author'>Date 2022</p>
+                    <h3 className='article-title'>{props.title}</h3>
+                    <p className='article-date'>{props.date}</p>
                 </div>
                 <div className='flex'>
-                    <p className='article-author'>by Author Here</p>
-                    <p className='article-author'>Section</p>
+                    <p className='article-author'>by {props.author}</p>
+                    <p className='article-section'>Section</p>
                 </div>
 
             </section>
--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { DATA_OBJ} from "../api/api_connection"
+// import axios from 'axios';
 
 import Article from './Article'
-import Loading from './Loading'
 import Search from './Search'
 
 import "../styles/ArticleContainer.css"
 
 function ArticleContainer(props) {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        let temp = JSON.parse(JSON.stringify(DATA_OBJ));
+        setArticles(temp);
+    }, []);
+
+    console.log(articles);
+
     return (
         <main className='article-container'>
             <Search />
-            <Article />
-            <Article />
-            <Loading />
-            <Loading />
+            <div className='article-container-display'>
+                { articles.length > 0 ? (
+                    // PROBLEM HERE: will not display article because each article is a JS object, need to compare to shower thoughts and see what happens there. How to parse properly so that the data can be gathered?
+                    // try to set up auth headers and actually get the real data from wordpress site with your password stuff
+                    articles.map((article) => {
+                        return (
+                            <Article 
+                            key={article.id} 
+                            title={article.title.rendered}
+                            author={article.author}
+                            date={article.date}
+                            link={article.link} />
+                        )
+                    })
+                ) : (
+                    <div className='no-articles'>
+                        <h3>No articles found!</h3>
+                        <p>Please try again later.</p>
+                    </div>
+                )}
+            </div>
         </main>
     );
 }
