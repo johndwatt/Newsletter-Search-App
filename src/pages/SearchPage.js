f161dcdcc8b0ed837bb0653636daf5796b7e68ae
johndwatt <jdw23@protonmail.com>
2022-08-12 13:52:33 -0700

    removes unused vars

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { API_URL } from "../api/api_connection";
 
 import Article from '../components/Article';
